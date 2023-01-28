709a7a29d9aff82490aecc3dd388dca6baeb1b44
zhoujc <1258349367@qq.com>
2023-01-28 23:06:11 +0800

    feat:使用craco配置别名和less预处理器

---
+++ craco.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const CracoLessPlugin = require('craco-less');
+
+const resolve = dir => path.join(__dirname, dir)
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+  webpack: {
+    alias: {
+      "@": resolve('src'),
+      "components": resolve('src/components'),
+      "utils": resolve('src/utils'),
+    }
+  }
+}
